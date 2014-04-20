0e3531841ccaa9599c3ed1e7c37f0c7c1c05f080
Spencer Barton <sbarton272@gmail.com>
2014-04-20 11:23:26 -0400

    Updated design.txt to include how we decided to split-up the work

--- lab/serial.h
+++ lab/serial.h
@@ -10,7 +10,27 @@
 #ifndef _SERIAL_H
 #define _SERIAL_H
 
+/*==================================
+ * Includes
+ *==================================*/
+
 #include <stdint.h>
+
+/*==================================
+ * MACROS
+ *==================================*/
+
+/*==================================
+ * Exernal Globals
+ *==================================*/
+
+/*==================================
+ * Local Globals
+ *==================================*/
+
+/*==================================
+ * Public Functions
+ *==================================*/
 
 /*
  * Configures serial communications, using the given baud prescale.
@@ -26,5 +46,9 @@ void serialWrite(char[] buffer, uint8_t len);
  * Reads len bytes from serial into the given buffer.
  */
 void serialRead(char[] buffer, uint8_t len);
+
+/*==================================
+ * Private Functions
+ *==================================*/
 
 #endif // _SERIAL_H
